b323c4272d1b085602c3cb4a8fc079076d3beffe
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-15 16:44:18 +0530

    Added: Monthly Employee Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -6,28 +6,32 @@ public class EmployeeWageComputation {
     public static final int WAGE_PER_HR = 20;
     public static final int FULL_TIME_HR = 8;
     public static final int PART_TIME_HR = 4;
+    public static final int WORK_DAY_PER_MONTH = 20;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-//        checking random value between 0 to to 2
-        int empCheck = (int)(Math.floor(Math.random()*10)%3);
+        int totalWorkHr = 0;
 
-//        checking employee is full-time or part-time or absent
-        switch (empCheck){
-            case IS_FULL_TIME:
-                System.out.println("Employee is Present and is Full Time...");
-                System.out.println("Full Day Wage is: "+( WAGE_PER_HR * FULL_TIME_HR ));
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Present and is Part Time...");
-                System.out.println("Full Day Wage is: "+( WAGE_PER_HR * PART_TIME_HR ));
-                break;
-            default:
-                System.out.println("Employee is Absent...");
-                System.out.println("Full Day Wage is: "+( 0 ));
-                break;
+        for (int day = 1; day<=WORK_DAY_PER_MONTH; day++) {
+
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+            switch (empCheck){
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present at Day "+day+" and is Full Time...");
+                    totalWorkHr = totalWorkHr + FULL_TIME_HR;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present at Day "+day+" and is Part Time...");
+                    totalWorkHr = totalWorkHr + PART_TIME_HR;
+                    break;
+                default:
+                    System.out.println("Employee is Absent at "+day);
+                    break;
+            }
         }
+        System.out.println("Employees Monthly Wage is: "+( WAGE_PER_HR * totalWorkHr ));
     }
 }
