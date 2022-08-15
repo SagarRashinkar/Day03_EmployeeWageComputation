20af34055a0b05f6941308361eb126595286d895
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-15 15:54:02 +0530

    Added: Part Time Employee Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -3,18 +3,23 @@ public class EmployeeWageComputation {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int WAGE_PER_HR = 20;
-        int FULL_DAY_HR = 8;
+        int FULL_TIME_HR = 8;
+        int PART_TIME_HR = 4;
 
-        int empCheck = (int)(Math.floor(Math.random()*10)%2);
+        int empCheck = (int)(Math.floor(Math.random()*10)%3);
 
-        if(empCheck == IS_PRESENT){
-            System.out.println("Employee is present...");
-            System.out.println("Full Day Wage is: "+( WAGE_PER_HR * FULL_DAY_HR ));
+        if(empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present and is Full Time...");
+            System.out.println("Full Day Wage is: "+( WAGE_PER_HR * FULL_TIME_HR ));
+        } else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Present and is Part Time...");
+            System.out.println("Full Day Wage is: "+( WAGE_PER_HR * PART_TIME_HR ));
         }else {
-            System.out.println("Employee is absent...");
-            System.out.println("Absent Day Wage is: "+ 0 );
+            System.out.println("Employee is Absent...");
+            System.out.println("Full Day Wage is: "+( 0 ));
         }
     }
 }
