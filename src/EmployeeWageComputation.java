67a9c4fd65f98d3a1d71d44e1e748071bbd42733
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-15 15:27:14 +0530

    Updated: Attendence Spacing in code

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int IS_PRESENT = 1;
